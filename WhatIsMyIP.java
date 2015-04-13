b542220c97dc3f30e9dc6f08bfdc30a4da4c6da7
Taha Emara <tahaemarah@yahoo.com>
2015-04-13 14:14:11 +0400

    Update WhatIsMyIP.java

--- WhatIsMyIP.java
+++ WhatIsMyIP.java
@@ -3,7 +3,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.URL;
+import java.util.Enumeration;
 
 /**
  *
